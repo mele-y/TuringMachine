474bf44094102a5a445ba42de9747d9036cc4a9a
mele-y <1275447710@qq.com>
2020-10-30 19:08:57 +0800

    update function initialTM

--- TuringMachine/tm.h
+++ TuringMachine/tm.h
@@ -4,17 +4,29 @@
 #include<QSet>
 #include<QString>
 #include<QMap>
-
-struct current
+#include<QTextDocument>
+#include<QTextBlock>
+struct Current
 {
     QString state;
     QChar header_symbol;
+    current(QString state,QChar header_symbol)
+    {
+        this->state=state;
+        this->header_symbol=header_symbol;
+    }
 };
-struct trans
+struct Trans
 {
     QString trans_state;
     QChar  replace_symbol;
-    int direction;//0 means left,1 means right
+    QChar direction;//0 means left,1 means right
+    Trans(QString trans_state,QChar replace_symbol,QChar direction)
+    {
+        this->trans_state=trans_state;
+        this->replace_symbol=replace_symbol;
+        this->direction=direction;
+    }
 };
 
 class TM
@@ -22,9 +34,10 @@ class TM
 private:QSet<QString> state_set;
         QChar blank_symnol='B';
         QMap<current,trans> rules;
+        QString final_state="pf";
 public:
     TM();
-    void initialTM(QString);
+    void initialTM(QTextDocument* text);
 
 };
 
